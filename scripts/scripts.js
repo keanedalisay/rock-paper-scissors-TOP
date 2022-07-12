01d60e6a060c2b110363c547ceb9b8b7aa44014b
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-07-12 12:50:04 +0800

    feat: add guide comments for Round Simulation function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,7 +8,20 @@
 
     randomly */
 
+// Create a function to simulate a round of Rock Paper Scissors
+/* It should take in two parameters
 
+    The players selection
+    The computers selection (which is the first function above)
+
+*/
+
+    /* It should return a string that
+
+        says either You Win
+        or You Lose
+
+    */
 
 
 
