9db97388f4d2ef31a7a2e5146fa3223a3b322bd2
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-07-24 13:23:35 +0800

    feat: target div.scoreBoard & score of player and  ai

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,6 +10,11 @@ setTimeout(function() {hostDisplay.textContent = 'Whoever wins 5 points wins the
 setTimeout(function() {hostDisplay.textContent = 'Are you ready?'}, 8000);
 setTimeout(function() {hostDisplay.textContent = 'Let the game begin!'}, 11000);
 
+const playerBoard = document.querySelector('div.playerScore');
+const aiBoard = document.querySelector('div.aiScore')
+let playerScore = document.querySelector('div.playerScore > .score > span');
+let aiScore = document.querySelector('div.aiScore > .score > span');
+
 const playerDisplay = document.querySelector('div.playerChoice');
 const playerChoice = document.querySelector('div.playerChoice > object');
 
@@ -18,5 +23,5 @@ const aiChoice = document.querySelector('div.aiChoice > object');
 
 const buttons = document.querySelectorAll('div.buttonBoard > button');
 buttons.forEach((button) => {
-    setTimeout(function() {button.addEventListener('click', playRound)}, 11000);
-})
+    setTimeout(function() {button.addEventListener('click', playRound)}, 000);
+}) 
