39c75f1ed3f80b64fc70a9f7769ec1114262e97c
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-07-24 10:04:13 +0800

    feat: add variables to target player & ai display choice

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,4 +9,36 @@ function computerPlay(){
     return objectInPlay[randomObject];
 }
 
+const playerDisplay = document.querySelector('div.playerChoice');
+const playerChoice = document.querySelector('div.playerChoice > object');
 
+const aiDisplay = document.querySelector('div.aiChoice');
+const aiChoice = document.querySelector('div.aiChoice > object');
+
+function playRound(e){
+    const playerPlay = this.getAttribute('class');
+    console.log(playerDisplay);
+    if (playerPlay === 'rock'){
+        playerDisplay.classList.add('rockBlue');
+        playerDisplay.classList.remove('paperRed');
+        playerDisplay.classList.remove('scissorsYellow');
+        playerChoice.setAttribute('data', 'img/rock.svg');
+        playerChoice.setAttribute('aria-label', 'rock');
+        playerChoice.textContent = 'Rock';
+    } else if (playerPlay === 'paper'){
+        playerDisplay.classList.add('paperRed');
+        playerDisplay.classList.remove('rockBlue');
+        playerDisplay.classList.remove('scissorsYellow');
+        playerChoice.setAttribute('data', 'img/paper.svg');
+        playerChoice.setAttribute('aria-label', 'paper');
+        playerChoice.textContent = 'Paper';
+    } else if (playerPlay === 'scissors'){
+        playerDisplay.classList.add('scissorsYellow');
+        playerDisplay.classList.remove('rockBlue');
+        playerDisplay.classList.remove('paperRed');
+        playerChoice.setAttribute('data', 'img/scissor.svg');
+        playerChoice.setAttribute('aria-label', 'scissors');
+        playerChoice.textContent = 'Scissors';
+    } return
+    
+}
