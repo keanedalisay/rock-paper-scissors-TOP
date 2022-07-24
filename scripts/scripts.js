f4a8bc26483d937a3d074cd0eea19fc82f2f2308
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-07-24 15:33:42 +0800

    refactor: organize timeouts & add var aiSelection

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,9 @@
-function computerPlay(){
+function aiPlay(){
     const objectInPlay = ['rock', 'paper', 'scissors'];
     const randomObject = Math.floor(Math.random() * 3);
     return objectInPlay[randomObject];
 }
+const aiSelection = aiPlay();
 
 const hostDisplay = document.querySelector('div.hostCard > .hostText');
 hostDisplay.textContent = 'Welcome to Rock Paper Scissors!';
@@ -22,27 +23,29 @@ const playerChoice = document.querySelector('div.playerChoice > object');
 const aiDisplay = document.querySelector('div.aiChoice');
 const aiChoice = document.querySelector('div.aiChoice > object');
 
-setTimeout(function() {playerBoard.classList.remove('playerSide')}, 5000)
-setTimeout(function() {playerDisplay.classList.remove('playerSide')}, 5000)
-
-setTimeout(function() {aiBoard.classList.add('aiSide')}, 5000);
-setTimeout(function() {aiDisplay.classList.add('aiSide')}, 5000);
-setTimeout(function() {aiBoard.classList.remove('aiSide')}, 11000);
-setTimeout(function() {aiDisplay.classList.remove('aiSide')}, 11000);
+setTimeout(function() {
+    playerBoard.classList.remove('playerSide');
+    playerDisplay.classList.remove('playerSide');
+    aiBoard.classList.add('aiSide');
+    aiDisplay.classList.add('aiSide');
+}, 5000)
 
 const rockButton = document.querySelector('div.buttonBoard > button.rock');
 const paperButton = document.querySelector('div.buttonBoard > button.paper');
 const scissorsButton = document.querySelector('div.buttonBoard > button.scissors');
 
-setTimeout(function() {rockButton.addEventListener('click', playRound)}, 11000);
-setTimeout(function() {paperButton.addEventListener('click', playRound)}, 11000);
-setTimeout(function() {scissorsButton.addEventListener('click', playRound)}, 11000);
-setTimeout(function() {playerDisplay.classList.add('selecting')}, 11000);
+setTimeout(function() {
+    playerDisplay.classList.add('selecting');
+    aiBoard.classList.remove('aiSide');
+    aiDisplay.classList.remove('aiSide');
+    rockButton.addEventListener('click', playRound);
+    paperButton.addEventListener('click', playRound);
+    scissorsButton.addEventListener('click', playRound);
+}, 11000);
 
 function ai(e){
-    const aiPlay = computerPlay();
     
-    if (aiPlay === 'rock'){
+    if (aiSelection === 'rock'){
         aiDisplay.classList.add('rockBlue');
         aiDisplay.classList.remove('paperRed');
         aiDisplay.classList.remove('scissorsYellow');
@@ -52,7 +55,7 @@ function ai(e){
         aiChoice.textContent = 'Rock';
 
         aiDisplay.classList.remove('selecting')
-    } else if (aiPlay === 'paper'){
+    } else if (aiSelection === 'paper'){
         aiDisplay.classList.add('paperRed');
         aiDisplay.classList.remove('rockBlue');
         aiDisplay.classList.remove('scissorsYellow');
@@ -60,7 +63,7 @@ function ai(e){
         aiChoice.setAttribute('data', 'img/paper.svg');
         aiChoice.setAttribute('aria-label', 'paper');
         aiChoice.textContent = 'Paper';
-    } else if (aiPlay === 'scissors'){
+    } else if (aiSelection === 'scissors'){
         aiDisplay.classList.add('scissorsYellow');
         aiDisplay.classList.remove('rockBlue');
         aiDisplay.classList.remove('paperRed');
